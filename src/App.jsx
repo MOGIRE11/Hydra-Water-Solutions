207c6079274a9dfd2eec125da854818d7b4a31e3
Your Name <you@example.com>
2024-03-16 17:07:13 +0300

    Initializing the Hydra Water Solutions Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl">We are back</h1>
+      <p className="bg-blue-700 text-right">Hawaninotice </p>
+    </>
+  )
+}
+
+export default App
