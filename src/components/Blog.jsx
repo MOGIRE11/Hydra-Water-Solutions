ec99dae06e4a112927f4b6834375cb6fa2142d58
brian <muriikibrian9@gmail.com>
2024-03-19 19:24:41 +0300

    blog message

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,6 +2,7 @@ function Blog() {
   return (
     <div className="px-4 lg:px-20 h-[70vh]">
       <h1 className="text-3xl text-green-500">The Blog page for Hydra</h1>
+      <h5>kesi tu</h5>
     </div>
   );
 }
