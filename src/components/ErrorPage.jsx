69b3d12e059cade52711cf097b88cc4556acbb51
brian <muriikibrian9@gmail.com>
2024-03-19 17:17:09 +0300

    developed the default error page for hydra

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,21 @@
+import { NavLink } from "react-router-dom"; 
 function ErrorPage() {
   return (
-    <div className="px-4 lg:px-20 h-[70vh]">
-      <h1 className="text-3xl text-red-900 ">
-        This is the error page for hydra
+    <div className="mt-16 min-h-screen">
+      <h1 className="font-bold text-4xl text-center mb-2">Ooops !</h1>
+      <h1 className="text-blue-800 text-4xl font-bold text-center mb-2 ">
+        Nothing
       </h1>
+      <h1 className="text-4xl font-bold text-black text-center mb-2">
+        Here <span className="text-green-500">...</span>
+      </h1>
+      <p className=" text-sm  lg:text-base  text-center w-1/2 mx-auto mb-4">
+        We can't seem to find the page you are looking for. Try going back to
+        the previous page or contact us for more information.
+      </p>
+
+      <button className="bg-green-500 py-2 px-4 rounded-md text-center text-white mx-auto flex mb-10 "> <NavLink to="/"> Go Back Home</NavLink>
+      </button>
     </div>
   );
 }
