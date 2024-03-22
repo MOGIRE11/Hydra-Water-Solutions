fc5a4886abd0d63ef816ccae74831bcc13dd067e
Your Name <mmogire11@gmail.com>
2024-03-22 23:56:28 +0300

    code cleanup , changed to a more defined directory for easier deployment

---
+++ frontend/script.js
@@ -0,0 +1,14 @@
+// let $(document).ready(function () {
+//   const intervalTime = 5000;
+//   let currentPage = 1;
+
+//   function autoRotate() {
+//     const element = $(`label[for="slide_2_${currentPage}"]`);
+//     if (element.length) {
+//       element.click();
+//     }
+//     currentPage = currentPage === 4 ? 1 : currentPage + 1;
+//   }
+
+//   setInterval(autoRotate, intervalTime);
+// });
