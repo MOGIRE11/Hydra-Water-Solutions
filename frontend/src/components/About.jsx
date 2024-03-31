690bfbf9428a7256209dccd24d021279fb67abb3
Your Name <mmogire11@gmail.com>
2024-03-31 17:56:57 +0300

    removed user photo from the about page of hydra

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import hydrateam from "./assets/Hydra-team-photo.svg";
-import user from "./assets/user.jpg";
+// import user from "./assets/user.jpg";
 import celestialdental from "./assets/celestial-dental-partner.svg";
 import fineteklabs from "./assets/fineteklabs-partner.png";
 import amario from "./assets/amario-partner.svg";
