77ffdb3b84207aeb781ff99218b190cf4c9fe604
brian <muriikibrian9@gmail.com>
2024-04-04 14:27:19 +0300

    breadcrumbs for product page

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,9 +1,18 @@
+import { NavLink } from "react-router-dom";
 function Products() {
   return (
     <div className="px-4 lg:px-20 min-h-[80vh]">
-      <h1 className="text-3xl text-green-500">The product page for Hydra</h1>
+      {/* Breadcrumb */}
+      <div className="breadcrumb flex items-center gap-2 text-xs mt-4 mb-8">
+        <NavLink to="/">
+          <i className="fa-solid fa-house mr-1 cursor-pointer "></i>
+          <p className="inline hover:text-blue-800 hover:font-bold">Home</p>
+        </NavLink>
+        <i className="fa-solid fa-chevron-right"></i>
+        Products
+      </div>
     </div>
   );
 }
 
-export default Products
+export default Products;
