e191d7136282e3420017354db384e40cf555581f
Your Name <mmogire11@gmail.com>
2024-04-05 15:29:09 +0300

    abou page code cleanup

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-// import user from "./assets/user.jpg";
 import celestialdental from "./assets/celestial-dental-partner.svg";
 import fineteklabs from "./assets/fineteklabs-partner.png";
 import amario from "./assets/amario-partner.svg";
@@ -37,24 +36,12 @@ function About() {
 
       {/* history */}
       <div className="section1 flex flex-col md:flex-row gap-8 mt-8 items-center justify-center">
-        {/* <img
-          src={hydrateam}
-          alt="hydra-team-photo"
-          className="h-[300px] w-full object-cover"
-        /> */}
         <div className="about-details md:w-[90%]">
           {/* heading */}
           <h2 className="font-bold underline underline-offset-4 mb-2">
             Frustrated by unreliable water treatment options?
           </h2>
           <p className="">
-            {/* Hydra was founded to address the notable lack of service delivery in
-            the areas of water, renewable energy and stand-by power solutions.
-            After identifying the gap in quality service, we sought to close the
-            accessibility and availability of safe drinking water to all while
-            encouraging cooperation with our partners. By offering top-notch
-            products and services, we hope to improve the communities we serve
-            as well as the environment . */}
             Hydra was born out of the same frustration. We saw a gap in quality
             service for water, renewable energy, and backup power solutions. Our
             mission? To bridge that gap and make these essentials accessible to
@@ -62,9 +49,6 @@ function About() {
           </p>
           <p className="my-4">
             <span className="font-bold ">We go beyond just products.</span>
-            {/* We are an organization aimed at providing solutions for the people
-            we serve through use of sophisticated technology bettering the
-            peoples overall health. */}
             We believe in building partnerships to ensure everyone has access to
             clean drinking water. By offering top-of-the-line technology and
             unparalleled service, we empower communities and protect the
@@ -75,9 +59,6 @@ function About() {
               At Hydra, it&apos;s about more than solutions; it&apos;s about
               people.
             </span>
-            {/* We believe in treating our customers with respect and honesty, our
-            employees with value and dignity and our partners with integrity and
-            ethical business practices. */}
             <span> We&apos;re committed to:</span>
             <ul className="">
               <li className="mt-1">
@@ -113,7 +94,6 @@ function About() {
       <div className="vision-wrapper grid grid-cols-1  md:w-[90%] mx-auto md:grid-cols-3 gap-10 mt-24 divide-y-20">
         {/* vision */}
         <div className="vision ">
-          {/* paper plane - telegram */}
           <hr className="my-1" />
           <div className="div flex items-center justify-center">
             <i className="fa-solid fa-paper-plane mr-2 cursor-pointer  w-8 h-8 text-white border flex items-center justify-center rounded-full bg-blue-700"></i>
@@ -131,7 +111,6 @@ function About() {
 
         {/* mission */}
         <div className="mission">
-          {/* compass */}
           <hr className="my-1" />
           <div className="div flex items-center justify-center">
             <i className="fa-solid fa-compass mr-2 cursor-pointer  w-8 h-8 text-white border flex items-center justify-center rounded-full bg-blue-700"></i>
@@ -151,7 +130,6 @@ function About() {
 
         {/* strengths */}
         <div className="strengths">
-          {/* leaf*/}
           <hr className="my-1" />
           <div className="div flex items-center justify-center">
             <i className="fa-solid fa-leaf mr-2 cursor-pointer  w-8 h-8 text-white border flex items-center justify-center rounded-full bg-blue-700"></i>
